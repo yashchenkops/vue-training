31dc6141185ce34189f29d0a642a19b021278137
Pavel <yashchenkodev@gmail.com>
2026-02-14 21:27:04 +0200

    feat: add editing post

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <div @click="isModalOpen = false" v-if="isModalOpen" class="modal-overlay">
+  <div v-if="isModalOpen" class="modal-overlay">
     <div class="modal bg-white border border-gray-200 p-4 mb-4">
       <form action="#">
         <div class="mb-4">
-          <input v-model="post.title" class="p-4 border border-gray-200 w-full" type="text" placeholder="title" />
+          <input v-model="editedPost.title" class="p-4 border border-gray-200 w-full" type="text"
+            placeholder="Title..." />
         </div>
         <div class="mb-4">
-          <textarea v-model="post.message" class="p-4 border border-gray-200 w-full min-h-20"
-            placeholder="content"></textarea>
+          <textarea v-model="editedPost.message" class="p-4 border border-gray-200 w-full min-h-20"
+            placeholder="Message..."></textarea>
         </div>
         <div>
-          <a @click.prevent="storePost" href="#"
+          <a @click.prevent="editPost" href="#"
             class="inline-block px-3 py-2 bg-sky-600 border border-sky-700 text-white">Store post</a>
         </div>
       </form>
@@ -21,11 +22,11 @@
     <div class="bg-white border border-gray-200 p-4 mb-4">
       <form action="">
         <div class="mb-4">
-          <input v-model="post.title" class="p-4 border border-gray-200 w-full" type="text" placeholder="title" />
+          <input v-model="post.title" class="p-4 border border-gray-200 w-full" type="text" placeholder="Title..." />
         </div>
         <div class="mb-4">
           <textarea v-model="post.message" class="p-4 border border-gray-200 w-full min-h-20"
-            placeholder="content"></textarea>
+            placeholder="Message..."></textarea>
         </div>
         <div>
           <a @click.prevent="storePost" href="#"
@@ -69,6 +70,10 @@ const post = reactive({
   title: '',
   message: '',
 });
+const editedPost = reactive({
+  title: '',
+  message: '',
+});
 
 const storePost = () => {
   posts.value.unshift({
@@ -84,6 +89,11 @@ const storePost = () => {
 
 const editPost = (post) => {
   isModalOpen.value = true;
+
+  Object.assign(editedPost, {
+    title: post.title,
+    message: post.message,
+  })
 };
 
 const deletePost = (post) => {
